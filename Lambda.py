1a9c486528c7031bdc1913ab01899f4aa0cca955
volkalik <57073759+volkalik@users.noreply.github.com>
2023-09-21 20:40:43 +0300

    init

--- Lambda.py
+++ Lambda.py
@@ -91,3 +91,44 @@ def vol(rad):
 
 v = vol(2)
 print(v)
+print('\n')
+
+
+print("""Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
+Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'\n""")
+
+def up_low(s):
+    d = {'upper': 0, 'lower': 0}
+
+    for char in s:
+        if char.isupper():
+            d['upper'] += 1
+        elif char.islower():
+            d['lower'] += 1
+        else:
+            pass
+
+    du = d['upper']
+    dl = d['lower']
+    print(f'No. of Upper case characters: {du}')
+    print(f'No. of Lower case Characters: {dl}')
+s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
+print(up_low(s))
+
+#print('\n')
+print('\n   Write a Python function that takes a list and returns a new list with unique elements of the first list.')
+def unique_list(lst):
+    lst2 = []
+    for n in lst:
+        if n not in lst2:
+            lst2.append(n)
+    print(lst2)
+#    for n in lst2:
+#        print(n)
+
+unique_list([1,1,1,1,2,2,3,3,3,3,4,5])
+
+
+#for n in lst:
+ #   if lst[n] == lst[n + 1]:
+  #      lst2.append(n)
