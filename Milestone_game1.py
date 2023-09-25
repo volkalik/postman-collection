79acf271aba158398d66e5e1fdb83a578607c71b
Alik Volk <alik.volk@dev.pro>
2023-09-25 22:07:01 +0300

    init

---
+++ Milestone_game1.py
@@ -0,0 +1,50 @@
+print("Game\n".upper())
+game_list = [0, 1, 2]
+
+
+def display_game(game_list):
+    print("Here is the current list:\n",
+          (game_list))
+
+
+display_game(game_list)
+
+
+def position_choice():
+    choice = 'wrong'
+    while choice not in ['0', '1', '2']:
+        choice = input('Pick a position (0,1,2): ')
+        if choice not in ['0', '1', '2']:
+            print("Sorry, invalid choice!")
+
+    return int(choice)
+
+
+position_choice()
+
+
+def replacement_choice(game_list, position):
+    user_placement = input("Type a string to place at position: ")
+    game_list[position] = user_placement
+    print(game_list)
+    return game_list
+
+
+replacement_choice(game_list, 2)
+
+
+def gameon_choice():
+    choice = 'wrong'
+    while choice not in ['Y', 'N']:
+        choice = input('Keep playing? (Y or N) ')
+        if choice not in ['Y', 'N']:
+            print("Sorry, not correct , please choose Y or N!")
+
+    if choice == "Y":
+        return True
+    else:
+        return False
+
+    return int(choice)
+
+print(gameon_choice())
