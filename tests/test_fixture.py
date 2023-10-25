c8d05e7a55037132738e413dfe7c0538aa1d2be3
Alik Volk <alik.volk@dev.pro>
2023-10-25 22:28:58 +0300

    init

---
+++ tests/test_fixture.py
@@ -0,0 +1,34 @@
+import pytest
+from OOP_1 import Dog, my_dog
+
+
+@pytest.fixture
+def bears():
+    family = ['Odnopup', 'Dobryun', 'Pyatochkkin', 'Doveryun']
+    return family
+
+
+def add_bears(bears):
+    new_bears = 'Security'
+    bears.append(new_bears)
+    assert bears == ['Odnopup', 'Dobryun', 'Pyatochkkin', 'Doveryun', 'Security']
+
+def test_add_bears(bears):
+    add_bears(bears)
+
+
+@pytest.fixture
+def some_data():
+    return int('50')
+
+def test_some_data(some_data):
+    assert some_data == 50
+
+@pytest.fixture
+def dog_bark():
+    return Dog(breed='york', name='Deny')
+
+def test_dog(dog_bark):
+    my_dog = dog_bark
+    assert my_dog.bark(7) == 'WOOF! My name is Deny and number is 7'
+
